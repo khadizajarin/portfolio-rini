e81ffa3842c5d2218cca1227b4ac6fd066b116c1
khadizajarin <khadiza131310@gmail.com>
2025-12-01 13:47:18 +0600

    Hero section final

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import { Inter, Vollkorn } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'], weight: ['400','600','700'] })
+const vollkorn = Vollkorn({ subsets: ['latin'], weight: ['400','600','700'] })
 
 export default function HeroSection() {
   return (
     <section className="w-full min-h-[80vh] flex items-center justify-center bg-[#F3E9DC]">
-      <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 items-center relative">
+      <div className="max-w-7xl w-full grid grid-cols-1 md:grid-cols-2 items-center relative">
 
         {/* LEFT TEXT */}
-        <div className="space-y-4 md:pl-6 md:relative md:z-10 md:translate-x-[100px] ">
-          <h1 className="text-5xl md:text-6xl  tracking-tight font-extrabold text-neutral-800">
+        <div className="space-y-2 md:relative md:z-10"
+             style={{ transform: 'translateX(6rem) translateY(-7rem)' }}>
+          <h1 className={`${vollkorn.className} text-8xl md:text-7xl tracking-tight font-extrabold text-neutral-600`}>
             Nigar Meherin Rini
           </h1>
           <p className="text-4xl text-[#9A3F3F] font-semibold">
@@ -16,7 +21,8 @@ export default function HeroSection() {
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className="flex justify-center md:justify-end md:relative md:z-0 md:translate-x-[-100px] ">
+        <div className="flex justify-center md:justify-end md:relative md:z-0"
+             style={{ transform: 'translateX(-6.25rem)' }}>
           <Image
             src="/images/image.png"
             alt="Portrait"
@@ -27,7 +33,8 @@ export default function HeroSection() {
         </div>
 
         {/* Bottom text */}
-         <div className="space-y-4 md:pl-6 md:absolute md:z-10 md:translate-x-[900px] md:translate-y-[200px] ">
+        <div className="space-y-4 md:absolute md:z-10"
+             style={{ transform: 'translate(70rem, 13.75rem)' }}>
           <p className="text-3xl font-semibold text-[#9A3F3F]">
             @journalist
           </p>
